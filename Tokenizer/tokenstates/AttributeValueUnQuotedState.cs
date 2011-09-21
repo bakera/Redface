517578e8399b41beaf220b2aef286995430f4185
MINAZUKI, Bakera <bakera@star.email.ne.jp>
2011-09-21 22:17:10 +0900

    add attr-unquoted state

--- Tokenizer/tokenstates/AttributeValueUnQuotedState.cs
+++ Tokenizer/tokenstates/AttributeValueUnQuotedState.cs
@@ -8,10 +8,45 @@ namespace Bakera.RedFace{
 		public class AttributeValueUnQuotedState : TokenizationState{
 
 			public override void Read(Tokenizer t){
-				throw new Exception("not implemented");
+				char? c = t.ConsumeChar();
 
-
+				switch(c){
+					case Chars.CHARACTER_TABULATION:
+					case Chars.LINE_FEED:
+					case Chars.FORM_FEED:
+					case Chars.SPACE:
+						t.ChangeTokenState<BeforeAttributeNameState>();
+						return;
+					case Chars.AMPERSAND:
+						t.AdditionalAllowedCharacter = Chars.GREATER_THAN_SIGN;
+						t.ChangeTokenState<CharacterReferenceInAttributeState>();
+						return;
+					case Chars.GREATER_THAN_SIGN:
+						t.ChangeTokenState<DataState>();
+						t.EmitToken();
+						return;
+					case Chars.NULL:
+						t.Parser.OnParseErrorRaised(string.Format("属性値にNUL文字が含まれています。"));
+						t.CurrentTagToken.CurrentAttribute.Value += Chars.REPLACEMENT_CHARACTER;
+						return;
+					case Chars.QUOTATION_MARK:
+					case Chars.APOSTROPHE:
+					case Chars.LESS_THAN_SIGN:
+					case Chars.EQUALS_SIGN:
+					case Chars.GRAVE_ACCENT:
+						t.Parser.OnParseErrorRaised(string.Format("引用符でくくられていない属性値の中に出現できない文字が出現しました。: {0}", c));
+						goto default;
+					case null:
+						t.Parser.OnParseErrorRaised(string.Format("属性値の解析中に終端に達しました。"));
+						t.UnConsume(1);
+						t.ChangeTokenState<DataState>();
+						return;
+					default:
+						t.CurrentTagToken.CurrentAttribute.Value += c;
+						return;
+				}
 			}
+
 		}
 	}
 }
