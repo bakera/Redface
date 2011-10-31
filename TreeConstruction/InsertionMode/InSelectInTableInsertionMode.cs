82e9becc3c28692d38c0d6f0b466f2fcf20a7e5a
MINAZUKI, Bakera <bakera@star.email.ne.jp>
2011-10-31 19:10:00 +0900

    add inselectintable

--- TreeConstruction/InsertionMode/InSelectInTableInsertionMode.cs
+++ TreeConstruction/InsertionMode/InSelectInTableInsertionMode.cs
@@ -5,10 +5,48 @@ namespace Bakera.RedFace{
 
 	public class InSelectInTableInsertionMode : InsertionMode{
 
-		public override void AppendToken(TreeConstruction tree, Token token){
-			Console.WriteLine("========\nnot implemented: {0} - {1}", this.Name, token);
-			tree.Parser.Stop();
-			return;
+		public override void AppendStartTagToken(TreeConstruction tree, StartTagToken token){
+			switch(token.Name){
+			case "caption":
+			case "table":
+			case "tbody":
+			case "tfoot":
+			case "thead":
+			case "tr":
+			case "td":
+			case "th":
+				OnParseErrorRaised(string.Format("select要素の中に出現できない要素です。: {0}", token.Name));
+				AppendEndTagToken(tree, new FakeEndTagToken(){Name = "select"});
+				tree.ReprocessFlag = true;
+				return;
+			}
+			AppendAnythingElse(tree, token);
+		}
+
+		public override void AppendEndTagToken(TreeConstruction tree, EndTagToken token){
+			switch(token.Name){
+			case "caption":
+			case "table":
+			case "tbody":
+			case "tfoot":
+			case "thead":
+			case "tr":
+			case "td":
+			case "th":
+				OnParseErrorRaised(string.Format("select要素の中に出現できない終了タグです。: {0}", token.Name));
+				if(tree.StackOfOpenElements.HaveElementInTableScope(token.Name)){
+					AppendEndTagToken(tree, new FakeEndTagToken(){Name = "select"});
+					tree.ReprocessFlag = true;
+					return;
+				}
+				return;
+			}
+			AppendAnythingElse(tree, token);
+		}
+
+
+		public override void AppendAnythingElse(TreeConstruction tree, Token token){
+			tree.AppendToken<InSelectInsertionMode>(token);
 		}
 
 	}
